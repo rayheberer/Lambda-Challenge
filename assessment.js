7b94720a6ab1391704b41433202397b3d3d44d9c
Ray Heberer <ray.heberer@gmail.com>
2018-03-02 10:32:06 +0800

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -7,7 +7,7 @@ After writing your function uncomment the matching function reference at the bot
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
+    return 'Hello World!';
 }
 
 /*
@@ -23,8 +23,16 @@ function helloWorld() {
              lambdaSchool(8); // returns 8
 */
 
-function lambdaSchool() {
-
+function lambdaSchool(num) {
+    if (num % 3 == 0 && num % 5 == 0) {
+        return 'Lambda School';
+    } else if (num % 5 == 0) {
+        return 'School';
+    } else if (num % 3 ==0) {
+        return 'Lambda';
+    } else {
+        return num;
+    }
 }
 
 /*
@@ -38,10 +46,19 @@ function lambdaSchool() {
              longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
-function longestString() {
+function longestString(strs) {
+    var longest_length = 0;
+    var longest_string = null;
 
+    strs.forEach(function(s) {
+        if (s.length > longest_length) {
+            longest_length = s.length;
+            longest_string = s;
+        }
+    });
+    
+    return longest_string;
 }
-
 /*
 4. Write a function called computeUserAverageAge that has a single parameter called users
      users is an array of user objects.
@@ -63,8 +80,15 @@ function longestString() {
              computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
-function computeUserAverageAge() {
+function computeUserAverageAge(users) {
+    var average_age = 0;
+    
+    users.forEach(function(u) {
+        average_age += u.age;
+    });
+    average_age /= users.length;
 
+    return Math.round(average_age);
 }
 
 module.exports = {
